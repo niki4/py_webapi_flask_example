38ec4024a6b289aea8b8a848aef2fbc1bebbc03a
Ivan Nikiforov <nikiforr@gmail.com>
2018-02-11 17:11:16 +0300

    Added error messages handling for Schema validation for POST

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, jsonify, request
-from marshmallow import Schema, fields, ValidationError
+from marshmallow import Schema, fields
 
 from db import PRODUCTS
 
@@ -20,7 +20,7 @@ def products_handle():
     if request.method == 'GET':
         search_query = request.args.get('q')
         products_to_show = PRODUCTS
-        if search_query is not None:
+        if search_query:
             products_to_show = [p for p in PRODUCTS if search_query.lower() in p['title'].lower()]
 
         is_only_in_store = 'only_in_store' in request.args
@@ -29,28 +29,29 @@ def products_handle():
 
         raw_from = request.args.get('from')
         raw_to = request.args.get('to')
-        if raw_from and raw_to and raw_from.isdigit() and raw_to.isdigit():
+        if (raw_from and raw_to) and (raw_from.isdigit() and raw_to.isdigit()):
             products_to_show = products_to_show[int(raw_from):int(raw_to)]
 
         raw_fields = request.args.get('fields')
         if raw_fields:
-            fields = raw_fields.split(',')
-            products_to_show = [{k: v for (k, v) in p.items() if k in fields} for p in products_to_show]
+            iter_fields = raw_fields.split(',')
+            products_to_show = [{k: v for (k, v) in p.items() if k in iter_fields} for p in products_to_show]
         return jsonify(products_to_show)
     elif request.method == 'POST':
-        request_data = json.loads(request.data.decode('utf-8'))
-        if is_product_data_valid(request_data):
+        errors, request_data = validate_product(request.data.decode('utf-8'))
+        if not errors:
             PRODUCTS.append(request_data)
             return jsonify('ok')
         else:
-            response = jsonify('data error')
+            response = jsonify(errors)
             response.status_code = 400
             return response
 
 
-def is_product_data_valid(product_data):
-    errors = ProductSchema().validate(product_data)
-    return not errors
+def validate_product(raw_data):
+    request_data = json.loads(raw_data)
+    errors = ProductSchema().validate(request_data)
+    return errors, request_data
 
 
 if __name__ == '__main__':
